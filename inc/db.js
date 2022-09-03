c890b31effd0eca421e590808679448e4b371742
Guilherme Mattos <91619478+GuilCMattos@users.noreply.github.com>
2022-09-03 14:35:45 -0300

    add: database embedded with Javascript

---
+++ inc/db.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2')
+
+const connection = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'user',
+  database: 'saboroso',
+  password: 'Saboroso10.'
+})
+
+module.exports = connection;
