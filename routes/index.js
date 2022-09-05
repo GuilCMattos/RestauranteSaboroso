13469136dc53127485a36b2668c1a1757f4d8bcd
Guilherme <mttsguil@gmail.com>
2022-09-05 09:55:23 -0300

    efect reservation with POST method for MySQL

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 
 var conn = require('./../inc/db');
 var express = require('express');
-var menus = require('./../inc/menus')
+var menus = require('./../inc/menus');
+var reservations = require('./../inc/reservations')
 var router = express.Router();
 
 /* GET home page. */
@@ -46,13 +47,40 @@ router.get('/menus', function(req,res,next){
 
 })
 
-router.get('/reservations', function(req,res,next){ 
+router.get('/reservations', function(req,res,next){
+  
+  reservations.render(req,res); 
 
-  res.render('reservations', { 
-    title: 'Reservas - Restaurante Saboroso!',
-    background: 'images/img_bg_2.jpg',
-    h1: 'Reserve uma Mesa!'
-  })
+})
+
+router.post('/reservations', function(req,res,next){ 
+
+  if(!req.body.name){ 
+    reservations.render(req,res, "Digite o nome!"); 
+   
+  } else if(!req.body.email) { 
+    reservations.render(req,res, "Digite o email!");
+  } else if(!req.body.people) { 
+    reservations.render(req,res, "Selecione o número de pessoas");
+  } else if(!req.body.date) { 
+    reservations.render(req,res, "Selecione a data");
+  } else if(!req.body.time) { 
+    reservations.render(req,res, "Selecione a hora");
+  } else { 
+
+    reservations.save(req.body).then(results => { 
+
+      req.body = {}
+
+      reservations.render(req,res, null, "Reserva realizada com sucesso");
+
+    }).catch(err=> { 
+      reservations.render(req, res, err.message);
+    });
+
+  }
+
+ 
 
 })
 
