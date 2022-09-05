36f0c88478dd4afa952f54f472b915a1f75f1350
Guilherme <mttsguil@gmail.com>
2022-09-05 11:14:42 -0300

    add: efect login in admin

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var users = require('./../inc/users')
 var router = express.Router();
 
 router.get("/", function(req, res, next){
@@ -7,13 +8,35 @@ router.get("/", function(req, res, next){
 
 });
 
+router.post("/login", function(req, res, next){
+
+    if(!req.body.email) { 
+        users.render(req,res, "Digite seu email")
+    } else if(!req.body.password) { 
+        users.render(req,res, "Preencha o campo senha")
+    } else { 
+
+        users.login(req.body.email, req.body.password).then(user=> { 
+
+            req.body = {};
+
+            req.session.user = user;
+
+            res.redirect("/admin")
+
+        }).catch(err=> { 
+
+            users.render(req,res, err.message)
+
+        });
+
+    }
+
+});
+
 router.get("/login", function(req, res, next){
 
-    if(!req.session.views) req.session.views = 0;
-
-    
-
-    res.render("admin/login");
+    users.render(req,res, null)
 
 });
 
