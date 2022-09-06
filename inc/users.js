7b06d01dda0a3330990ddec0da499b4d939f69cc
Guilherme <mttsguil@gmail.com>
2022-09-06 12:52:56 -0300

    add: admin users creat routes

--- inc/users.js
+++ inc/users.js
@@ -50,6 +50,91 @@ module.exports = {
             })
 
         });
+    },
+
+    save(fields) { 
+
+        return new Promise((resolve, reject)=> { 
+          
+            let query, params = [
+                fields.name,
+                fields.email,
+                fields.password
+            ];
+
+            if(parseInt(fields.id) > 0) {
+
+                query = `
+                UPDATE tb_users
+                SET 
+                    name = ?,
+                    email = ?,
+                    password = ?
+                WHERE id = ?
+                `
+               params.push(fields.id)
+
+            } else { 
+
+                query = `
+                INSERT INTO tb_users (name, email, password)
+                VALUES(?, ?, ?)
+                `;
+            }
+
+            conn.query(query, params, (err, results)=> {
+
+            if(err) { 
+                reject(err)
+            } else {
+
+                resolve(results)
+            }
+
+        });
+
+        });
+
+    },
+
+    getUsers() { 
+
+        return new Promise((resolve, reject)=> { 
+
+            conn.query(`
+                SELECT * FROM tb_users ORDER BY title
+                `, (err, results)=> { 
+
+                    if(err) { 
+                    reject(err)
+                    } 
+
+                  resolve(results)
+
+                });
+
+        });
+    },
+
+    delete(id) { 
+
+        return new Promise((resolve, reject)=> { 
+
+            conn.query(` 
+                DELETE FROM tb_users WHERE id = ?
+            `, [
+                id
+            ], (err, results)=> { 
+
+                if(err) { 
+                    reject(err);
+                } else { 
+                    resolve(results)
+                }
+
+            });
+        })
+
     }
 
 }
