d1e5bef643f8a251a42dc8bdb6943d61b60853ec
Guilherme <mttsguil@gmail.com>
2022-09-06 09:36:22 -0300

    add: admin menu - alter datas via POST

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ var form = formidable.IncomingForm({
 
   form.parse(req, function(err, fields, files){
 
+    req.body = fields
     req.fields = fields;
     req.files = files;
 
@@ -53,7 +54,7 @@ app.use(session({
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
