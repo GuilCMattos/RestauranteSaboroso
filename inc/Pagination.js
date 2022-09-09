691e7840d77edc73d306897df5815d4a540e6dd2
Guilherme <mttsguil@gmail.com>
2022-09-09 09:47:16 -0300

    add: working pagination, btn last and next

--- inc/Pagination.js
+++ inc/Pagination.js
@@ -91,6 +91,14 @@ class Pagination {
             nrend = this.getCurrentPage() - parseInt(limitPagesNav/2)
         }
 
+        if(this.getCurrentPage() > 1) { 
+            links.push({
+                text: '<',
+                href: '?' + this.getQueryString(Object.assign({}, params, { page: this.getCurrentPage() -1} ))
+                
+            })
+        }
+
 
         for(let x = nrstart; x <= nrend; x++) { 
             links.push({
@@ -98,6 +106,16 @@ class Pagination {
                 href: '?' + this.getQueryString(Object.assign({}, params, { page: x} )),
                 active: (x === this.getCurrentPage())
             })
+        }
+
+        if(this.getCurrentPage() < this.getTotalPages()) { 
+            links.push({
+                text: '>',
+                href: '?' + this.getQueryString(Object.assign({}, params, { page: this.getCurrentPage() + 1} ))
+                
+            })
+
+
         }
 
 
