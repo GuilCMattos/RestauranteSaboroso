ee4365d0c3da888994b69e02b8212b549ec3c505
Guilherme <mttsguil@gmail.com>
2022-09-09 10:42:48 -0300

    add: working with socket.io

--- app.js
+++ app.js
@@ -7,10 +7,20 @@ var session = require('express-session');
 var RedisStore = require('connect-redis')(session);
 var indexRouter = require('./routes/index');
 var adminRouter = require('./routes/admin');
-var formidable = require('formidable')
+var formidable = require('formidable');
+var http = require('http');
+var socket = require('socket.io')
 var path = require('path')
 
 var app = express();
+
+var http = http.Server(app);
+var io = socket(http);
+
+io.on('connection', function(socket){ 
+
+
+})
 
 app.use(function(req,res,next){
 
@@ -53,7 +63,6 @@ app.use(session({
 }))
 
 app.use(logger('dev'));
-app.use(express.json());
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -77,4 +86,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+http.listen(3000, function(){ 
+  console.log('servidor em execução...')
+})
+
+
