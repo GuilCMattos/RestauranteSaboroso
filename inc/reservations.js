7e09fbd047fb6a97c43ad845ab64d3e2a642debb
Guilherme <mttsguil@gmail.com>
2022-09-09 10:31:12 -0300

    add: server data and att grafics

--- inc/reservations.js
+++ inc/reservations.js
@@ -1,5 +1,7 @@
 var conn = require('./db');
 const Pagination = require('./Pagination');
+var moment = require('moment');
+const { format } = require('morgan');
 
 module.exports = { 
 
@@ -130,6 +132,49 @@ module.exports = {
 
             });
         })
+
+    }, 
+
+    chart(req) {
+
+        return new Promise((resolve, reject)=> { 
+
+            conn.query(`
+            SELECT
+            CONCAT(YEAR(date), '-', MONTH(date)) AS date,
+            COUNT(*) AS total,
+            SUM(people) / COUNT(*) AS avg_people
+            FROM tb_reservations
+            WHERE
+                date BETWEEN ? AND ?
+            GROUP BY CONCAT(YEAR(date), '-' ,MONTH(date))
+            ORDER BY date DESC`, 
+            [
+                req.query.start,
+                req.query.end
+        ], (err, results) => { 
+            if(err) { 
+                reject(err);
+            } else { 
+
+                let months = [];
+                let values = [];
+
+                results.forEach(row => { 
+                    months.push(moment(row.date).format('MMM YYYY'))
+                    values.push(row.total)
+                });
+
+                resolve({ 
+                    months,
+                    values
+                })
+
+            }
+        }
+        )
+        });
+
 
     }
 
